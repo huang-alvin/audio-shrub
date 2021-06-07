77768cba982804e2271b8b9bdb48449413e59da2
alvin <huang.alvin.01@gmail.com>
2021-06-06 23:27:50 -0700

    merch, music post, songs

---
+++ app/seeds/music_posts.py
@@ -0,0 +1,32 @@
+from app.models import db, Song, Music_Post
+from .utils import song_list, music_post_image_list
+from faker import Faker
+import random
+
+faker = Faker()
+Faker.seed(0)
+# Adds a demo user, you can add other users here if you want
+def seed_music_posts():
+
+    price_list = [0,5,10,15]
+    for user_id in range(1,60):
+        num_words = random.randint(3,6)
+        num_sentences = random.randint(2,4)
+        title_words = faker.words(nb=num_words)
+
+        music_post = Music_Post(
+            user_id = user_id,
+            title = "".join(title_words),
+            description = faker.sentences(nb=num_sentences),
+            price= random.choice(price_list),
+            image= random.choice(music_post_image_list)
+        )
+    db.session.commit()
+
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and resets
+# the auto incrementing primary key
+def undo_music_posts():
+    db.session.execute('TRUNCATE music_posts RESTART IDENTITY CASCADE;')
+    db.session.commit()
---
+++ app/seeds/utils.py
@@ -0,0 +1,67 @@
+user_image_list = [
+    "https://townsquare.media/site/204/files/2017/02/the-band-perry-my-bad-imagination-shows.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
+    "https://i.ytimg.com/vi/SSq2nfv4Jkw/maxresdefault.jpg",
+    "https://s3.amazonaws.com/bit-photos/large/8821838.jpeg",
+    "https://www.yourobserver.com/sites/default/files/styles/sliders_and_planned_story_image_870x580/public/261667_standard.jpeg",
+    "https://townsquare.media/site/623/files/2019/12/brent-cobb.jpg?w=980&q=75",
+    "https://www.shindig-magazine.com/wp-content/uploads/2019/11/Shadow-Show_3_Colleen-Rose-1024x683.jpg",
+    "https://static01.nyt.com/images/2019/05/07/arts/07bikini-memory/05bikini-memory-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    "https://scottstacom.files.wordpress.com/2016/10/mg_2324-cover.jpg?w=840",
+    "https://tutsplus-media.s3.amazonaws.com/photo.tutsplus.com/uploads/2013/02/Storybooks-5.jpg",
+    "https://static.boredpanda.com/blog/wp-content/uploads/2019/02/funny-awkward-metal-bands-photos-fb1-png__700.jpg",
+    "https://diy-magazine.s3.amazonaws.com/d/diy/Artists/F/Friendly-Fires/_landscape/FriendlyFires_PressShots_HighRes-copy.jpg",
+    "https://syracusecommercialphotography.com/wp-content/uploads/2015/08/Band.jpg",
+    "https://i0.wp.com/www.wordsformusic.blog/wp-content/uploads/2020/07/chartreuse-conor-schofield.jpg?fit=768%2C767&ssl=1",
+    "https://oksessions.com/wp-content/uploads/2018/08/Jose-Hernandez-OKC-Singer-Songwriter.jpg",
+    "https://greenvillejournal.com/wp-content/uploads/2018/07/0713.GJ_.TomAngstBandProfile.JonahHunterSalBrunoDanielleMcConaghyAndPearsonParham-2.WillCrooks-e1530900828831.jpg",
+    "https://bmamag.com/wp-content/uploads/2018/07/Turner-Brown-Band-event-pic-667x500.jpg",
+    "https://expertphotography.com/wp-content/uploads/2020/06/band-photography-8.jpg",
+    "https://i.pinimg.com/originals/ac/b4/b7/acb4b7c8f686fdbbec7f19a3cac1ea44.jpg",
+    "https://pyxis.nymag.com/v1/imgs/547/061/d1d14ca2eac0a3ab06494f7114825d1dcd-31-rolling-stones-ex-1.rsquare.w330.jpg"
+]
+
+song_list = [
+   "https://audio-shrub.s3.amazonaws.com/audio-file/218801__valentinsosnitskiy__acoustic-guitar-country.mp3",
+   "https://audio-shrub.s3.amazonaws.com/audio-file/246315__dvideoguy__folk-guitar-music-track.wav",
+   "https://audio-shrub.s3.amazonaws.com/audio-file/257993__orangefreesounds__disco-funky-beat.wav",
+   "https://audio-shrub.s3.amazonaws.com/audio-file/342359__stankbeast__city-rap-beat.mp3",
+   "https://audio-shrub.s3.amazonaws.com/audio-file/347848__foolboymedia__new-york-jazz-loop.wav",
+   "https://audio-shrub.s3.amazonaws.com/audio-file/45725__torn-rugged-audio-35__atari-mono-03.wav",
+   "https://audio-shrub.s3.amazonaws.com/audio-file/495482__valentinsosnitskiy__guitar-jam-in-ableton-live.wav",
+   "https://audio-shrub.s3.amazonaws.com/audio-file/49658__sub-d__more-jazz-guitar.wav",
+   "https://audio-shrub.s3.amazonaws.com/audio-file/536202__badoink__dance-beat-fx-loop.wav",
+   "https://audio-shrub.s3.amazonaws.com/audio-file/64613__djgriffin__calculus-acid-n-kick-120bpm.wav",
+]
+
+music_post_image_list = [
+    "https://preview.redd.it/lxm0en8dop951.png?width=3000&format=png&auto=webp&s=fd16ff1cd77665b323a455d8b585a702b4e4ed36",
+    "https://1.bp.blogspot.com/-ISJ_-u2C6Yk/WYuCRq0MbdI/AAAAAAAAgNw/jGfZZdaBdbsG7D46KUa9BV005qJCfsbkQCLcBGAs/s1600/Visualartzi%2BInspiration.jpg",
+    "https://about.canva.com/wp-content/uploads/sites/3/2015/01/album-cover.png",
+    "https://64.media.tumblr.com/ab09dde560b93176efa9074a55c827e9/tumblr_ofeh3hlxzB1qloxj6o1_1280.jpg",
+    "https://i.pinimg.com/originals/26/11/79/261179df4ee3e789c7570eea3487700b.jpg",
+    "https://i.pinimg.com/736x/48/32/0e/48320e5c5d030ea53bec8cf3baed1494.jpg",
+    "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/listen-or-die-hip-hop-mixtape-album-cover-art-design-template-9bd12ab25677c125abc1c025c1d6612c_screen.jpg?ts=1588403549",
+    "https://lh3.googleusercontent.com/proxy/nKGdUTB2zZYALZenV9WEaw4jBhqn8DXLt0yDOT6Ord5W5RV5Clh8nsf0RoVKSZy51Y3MwRPM0VU29ijWg-sd5LVX3u3KYajN_sSkJa0uPJwTiDMOkfhjmI4",
+    "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/3fc12694456069.5e7f51e8ade82.jpg",
+    "https://www.designformusic.com/wp-content/uploads/2016/09/electro-synthwave-album-cover-500x500.jpg",
+    "https://c4.wallpaperflare.com/wallpaper/412/477/91/music-album-covers-cover-art-wallpaper-preview.jpg",
+    "https://cdn.pixabay.com/photo/2017/09/19/08/20/earth-2764473_1280.jpg",
+    "https://www.cryochamberlabel.com/wp-content/uploads/2018/12/Dark-Ambient-of-2018-610x581.jpg",
+    "https://wallpaper.dog/large/20458355.jpg",
+    "https://northerntransmissions.com/wp-content/uploads/2016/10/zzzz4653555.jpg",
+    "http://crownnote.com/sites/default/files/BN-UT554_QUEENS_8S_20170821123142.jpg"
+]
+
+merch_image_list = [
+    "https://d2h1pu99sxkfvn.cloudfront.net/b0/3702140/419034300_O9mhak5pWc/P0.jpg",
+    "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/122822616/original/2568672b8a1e75854a2fea821f86137110eb129d/design-your-music-album-covers.jpg",
+    "https://www.smallbusinessprinting.com/images/products_gallery_images/CD_Inserts3.jpg",
+    "https://bestrocktshirts.com/components/com_jshopping/files/img_products/2275_BIG_CREAM-BAND-shirt.jpg",
+    "https://pimg.bucklecontent.com/images/products/5280000BKD01V1/VYT/f/09f794a1c8f3d1469f50dd5bf31ea9cav3?quality=0.8&width=845",
+    "https://images-platform.99static.com//XlgfX1eEFg5Tbo2HNP6XDVjAXy0=/146x0:1106x960/fit-in/590x590/99designs-contests-attachments/102/102786/attachment_102786347",
+    "https://lastexittonowhere.imgix.net/uploads/feature_content/JAWS-CLAPPERBOARD-DESIGN-VINTAGE-STONEWASH-BLACK-TSHIRT-BY-LAST-_U6ziRWk.jpg?h=540&ixlib=django-1.2.0&w=535",
+    "https://cdn.shopify.com/s/files/1/0253/3716/7947/products/album-mockup.jpg?v=1569359820",
+    "https://ih1.redbubble.net/image.1129767438.7678/ur,pin_large_front,square,600x600.u1.jpg",
+    "https://i.pinimg.com/originals/06/cf/e7/06cfe7121093080db0213ea748e36a23.jpg",
+    "https://i.ebayimg.com/images/g/Yf8AAOSwFNtgb8AE/s-l300.jpg"
+]
